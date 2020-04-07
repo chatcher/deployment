ede746638bf9de13722b1ddc53de794563f21f70
Chad Hatcher <chatcher@users.noreply.github.com>
2020-04-07 13:15:30 -0700

    Update chromebook.sh

--- scripts/chromebook.sh
+++ scripts/chromebook.sh
@@ -1,12 +1,24 @@
+#!/usr/bin/env bash
+
+set -x
+
+echo "Installing a bunch of stuff"
 sudo apt update
 sudo apt install nano
 sudo apt install git
+sudo apt install gnupg
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# this might fail because nvm might not be available in the current shell
+nvm install 8
+nvm install 10
+nvm install 12
+
+echo
+echo "Configuring some stuff"
 mkdir .ssh && chmod 700 .ssh && ssh-keygen
-nvm install 8; nvm install 10; nvm install 12; nvm use 10
 cat .ssh/*pub
 git config --global user.name "Chad Hatcher"
 git config --global user.email "hackenslacker@gmail.com"
-sudo apt install gnupg
 gpg --full-generate-key
 gpg --list-secret-keys --keyid-format LONG
