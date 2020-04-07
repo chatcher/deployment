bec9e679b9f7163544776dc4eddd6b0aa57bddf2
Chad Hatcher <chatcher@users.noreply.github.com>
2020-04-06 22:13:41 -0700

    Update and rename chromebook.hstry to chromebook.sh

--- scripts/chromebook.sh
+++ scripts/chromebook.sh
@@ -1,9 +1,9 @@
+sudo apt update
 sudo apt install nano
 sudo apt install git
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install 8; nvm install 10; nvm install 12; nvm use 10
-sudo apt update
 mkdir .ssh && chmod 700 .ssh && ssh-keygen
+nvm install 8; nvm install 10; nvm install 12; nvm use 10
 cat .ssh/*pub
 git config --global user.name "Chad Hatcher"
 git config --global user.email "hackenslacker@gmail.com"
