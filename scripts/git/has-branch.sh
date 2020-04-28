fad677f94572d264b40373427181a49dfe07bab1
Chad Hatcher <hackenslacker@gmail.com>
2020-04-28 03:28:25 -0700

    has-branch: call has-local and has-remote, fix usage output

--- scripts/git/has-branch.sh
+++ scripts/git/has-branch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 usage() {
-	echo "  test whether <branch> exists on the remote" >&2
+	echo "  test whether <branch> exists locally or on the remote" >&2
 	echo "usage:" >&2
-	echo "  ${0#*/} <branch>" >&2
+	echo "  $(basename $0) <branch>" >&2
 
 	if ! [ -z "$1" ]; then echo "error: ${1}" >&2; fi
 
@@ -12,12 +12,4 @@ usage() {
 
 if [ -z "${1}" ]; then usage "<branch> required"; fi
 
-while read -u 3 line; do
-	clean="${line#* }"
-	branch="${clean#remotes/origin/}"
-	if [ "${branch}" == "${1}" ]; then
-		exit
-	fi
-done 3<<<"$(git branch -a)"
-
-exit 1
+./has-local-branch.sh "${1}" || ./has-remote-branch.sh "${1}"
