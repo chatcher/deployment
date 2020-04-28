02719f83bd870413940c5586c0500e68338030d7
Chad Hatcher <hackenslacker@gmail.com>
2020-04-28 07:43:15 -0700

    checkout-test: reorder tests and rely on checkout to clean branches

--- scripts/git/checkout-test.sh
+++ scripts/git/checkout-test.sh
@@ -3,15 +3,14 @@
 echo -e "\033[33m  no arg:\033[m"
 ./checkout.sh
 echo '-----'
+
+echo -e "\033[33m  new branch\033[m"
+branch="temp/$(date +%s)"
+./checkout.sh "${branch}"
+echo '-----'
+
 echo -e "\033[33m  master:\033[m"
 ./checkout.sh master
 echo '-----'
-echo -e "\033[33m  new branch\033[m"
-branch="temp-$(date +%s)"
-./checkout.sh "${branch}"
-echo '-----'
-echo -e "\033[90m  cleaning up...\033[m"
-git checkout master
-git branch -d "${branch}"
 
-
+git branch
