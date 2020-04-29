9d6defa483a315089418dfae27f240135e29ccfa
Chad Hatcher <chatcher@users.noreply.github.com>
2020-04-29 08:02:16 -0700

    Update init.sh

--- setup/catalina/init.sh
+++ setup/catalina/init.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+open 'https://download.docker.com/mac/stable/Docker.dmg'
+open 'https://desktop.github.com/'
+open 'https://www.sublimetext.com/3'
+open 'https://www.sublimemerge.com/'
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install nvm
+
+brew cask install homebrew/cask-versions/adoptopenjdk8
+
+brew install jenkins-lts
+#brew services start jenkins-lts
 
 nvm install 10
 nvm install 12
