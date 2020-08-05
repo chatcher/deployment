4fc1c507865fddbf8f9f546868a72e6e97d4b3f0
Chad Hatcher <hackenslacker@gmail.com>
2020-08-04 20:23:58 -0700

    install sublime text on chromebook

--- setup/chromebook/init.sh
+++ setup/chromebook/init.sh
@@ -2,7 +2,11 @@
 
 set -x
 
-echo "Installing a bunch of stuff"
+echo "Installing a bunch of stuff" >&2
+sudo apt update
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install -y nmap
 sudo apt install -y nano
@@ -11,6 +15,7 @@ sudo apt install -y gnupg
 sudo apt install -y jq
 sudo apt install -y dnsutils
 sudo apt install -y whois
+sudo apt install -y sublime-text
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # this might fail because nvm might not be available in the current shell
@@ -24,3 +29,5 @@ git config --global user.name "Chad Hatcher"
 git config --global user.email "hackenslacker@gmail.com"
 mkdir .ssh && chmod 700 .ssh && ssh-keygen
 cat .ssh/*pub
+
+
