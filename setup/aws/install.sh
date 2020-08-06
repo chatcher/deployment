85b4f5039e314f84646a219f20e56b3d8e54fd0e
Chad Hatcher <chatcher@users.noreply.github.com>
2020-08-05 20:30:26 -0700

    aws: install letsencrypt

--- setup/aws/install.sh
+++ setup/aws/install.sh
@@ -7,6 +7,7 @@ sudo apt update -y
 sudo apt install -y nmap
 sudo apt install -y jq
 sudo apt install -y nginx
+sudo apt install -y letsencrypt
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 
