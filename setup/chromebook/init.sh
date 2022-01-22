bb5613c35b84d57a8f51f3fbc700044b742ce0c9
Chad Hatcher <chatcher@users.noreply.github.com>
2022-01-22 09:38:05 -0800

    chrombook init: sublime-merge, node 14, .bashrc

--- setup/chromebook/init.sh
+++ setup/chromebook/init.sh
@@ -17,19 +17,32 @@ sudo apt install -y dnsutils
 sudo apt install -y rsync
 sudo apt install -y whois
 sudo apt install -y sublime-text
+sudo apt install -y sublime-merge
+mkdir "${HOME}/.nvm"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # this might fail because nvm might not be available in the current shell
 nvm install 8
 nvm install 10
 nvm install 12
+nvm install 14
 
 echo
-echo "Configuring some stuff"
+echo "Download .bashrc"
+if [ -f "${HOME}/.bashrc" ]; then
+	mv "${HOME}/.bashrc" "${HOME}/.bashrc.bak"
+fi
+curl https://raw.githubusercontent.com/chatcher/deployment/master/setup/chromebook/.bashrc > "${HOME}/.bashrc"
+
+echo >&2
+echo "Configuring some stuff" >&2
 git config --global user.name "Chad Hatcher"
 git config --global user.email "hackenslacker@gmail.com"
 git config --global diff.wsErrorHighlight all
 mkdir "${HOME}/.ssh" && chmod 700 "${HOME}/.ssh" && ssh-keygen
+
+echo >&2
 cat "${HOME}/.ssh/*pub"
 
-
+echo >&2
+echo -e "If that worked, then paste that into github or whatever." >&2
